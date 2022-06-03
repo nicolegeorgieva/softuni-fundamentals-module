a7218d5684a943e30fcf7ff89d0e6fdd73f43c46
Nicole <nicole.georgievva@gmail.com>
2022-06-03 21:45:03 +0300

    Add `reversedChars` task

---
+++ data-types-and-variables/exercise/reversedChars.js
@@ -0,0 +1,5 @@
+function reversedChars(firstCharacter, secondCharacter, lastCharacter) {
+    console.log(`${lastCharacter} ${secondCharacter} ${firstCharacter}`);
+}
+
+reversedChars('A', 'B', 'C');
