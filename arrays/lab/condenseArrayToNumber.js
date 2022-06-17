6e95886b24565b54289d158b5d9c0b37dbb202bc
Nicole <nicole.georgievva@gmail.com>
2022-06-17 16:21:35 +0300

    Add `condenseArrayToNumber` task

---
+++ arrays/lab/condenseArrayToNumber.js
@@ -0,0 +1,23 @@
+function condenseArrayToNumber(numbers) {
+    let result = [];
+
+    for (let el of numbers) {
+        result.push(el);
+    }
+
+    while (result.length > 1) {
+        let tempArr = [];
+
+        for (let i = 0; i < result.length - 1; i++) {
+            tempArr[i] = result[i] + result[i + 1];
+        }
+
+        result = tempArr;
+    }
+
+    console.log(result.join());
+}
+
+
+condenseArrayToNumber([2, 10, 3]);
+condenseArrayToNumber([5, 0, 4, 1, 2]);
