3d13867b15d0ac2e1b871d68a2a1291060ad988f
Nicole <nicole.georgievva@gmail.com>
2022-06-17 15:17:51 +0300

    Refactor code

--- arrays/lab/reverseInPlace.js
+++ arrays/lab/reverseInPlace.js
@@ -1,10 +1,10 @@
 function reverseInPlace(arr) {
     let k = arr.length - 1;
 
-    for (let i = 0; i < arr.length / 2; i++) {
+    for (let i = 0; i < arr.length / 2; i++, k--) {
         let temp = arr[i];
-        arr[i] = arr[k - i];
-        arr[k - i] = temp;
+        arr[i] = arr[k];
+        arr[k] = temp;
     }
 
     console.log(arr.join(' '));
