68d5adcc08a5e60a8096e299f7b47671494e2b73
Nicole <nicole.georgievva@gmail.com>
2022-06-17 14:33:00 +0300

    Add `reverseInPlace` task solution

---
+++ arrays/lab/reverseInPlace.js
@@ -0,0 +1,18 @@
+function reverseInPlace(arr) {
+    let k = arr.length - 1;
+
+    for (let i = 0; i < arr.length / 2; i++) {
+        let temp = arr[i];
+        arr[i] = arr[k - i];
+        arr[k - i] = temp;
+    }
+
+    console.log(arr.join(' '));
+}
+
+
+reverseInPlace(['a', 'b', 'c', 'd', 'e']);
+console.log('--------------------------');
+reverseInPlace(['abc', 'def', 'hig', 'klm', 'nop']);
+console.log('--------------------------');
+reverseInPlace(['33', '123', '0', 'dd']);
