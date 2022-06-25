6d2aaf1e7a35d6b1137b601628e861b0dea57024
Nicole <nicole.georgievva@gmail.com>
2022-06-25 18:04:02 +0300

    Add `lastKNumbersSequence` task

---
+++ arrays-advanced/lab/lastKNumbersSequence.js
@@ -0,0 +1,29 @@
+function lastKNumbersSequence(n, k) {
+    let result = [1];
+
+    for (let i = 1; i < n; i++) {
+        let nextElement = generateElement(previousNumbers(result, k));
+        result.push(nextElement);
+    }
+
+    function previousNumbers(arr, k) {
+        let arrCopy = arr.slice(0, arr.length)
+        return arrCopy.reverse().slice(0, k).reverse();
+    }
+
+    function generateElement(previousNumbersArr) {
+        let sumOfPreviousKNumbers = 0;
+
+        for (let i = 0; i < previousNumbersArr.length; i++) {
+
+            let currentElement = previousNumbersArr[i];
+            sumOfPreviousKNumbers += currentElement;
+        }
+
+        return sumOfPreviousKNumbers;
+    }
+
+    console.log(result.join(' '));
+}
+
+lastKNumbersSequence(6, 3);
