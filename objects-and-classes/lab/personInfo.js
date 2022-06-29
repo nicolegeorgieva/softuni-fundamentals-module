16ee194bf64501c17f2a34859d3c1c2ffe2b0a96
Nicole <nicole.georgievva@gmail.com>
2022-06-29 19:21:07 +0300

    Add `personInfo` task solution

---
+++ objects-and-classes/lab/personInfo.js
@@ -0,0 +1,9 @@
+function personInfo(firstName, lastName, age) {
+    return {
+        firstName,
+        lastName,
+        age
+    };
+};
+
+console.log(personInfo('Nicole', 'Georgieva', '23'));
