4f30903ab0d65dd82d428ab403f181e11d56da32
Nicole <nicole.georgievva@gmail.com>
2022-07-12 20:08:55 +0300

    Refactor code

--- functions/homework/nxnMatrix.js
+++ functions/homework/nxnMatrix.js
@@ -5,15 +5,20 @@ function nxnMatrix(number) {
 
     function createRowOfNumber(number) {
         let num = Number(number);
+
+        for (let j = 1; j <= num; j++) {
+            print(createRow(num));
+        }
+    }
+
+    function createRow(num) {
         let row = '';
 
         for (let i = 1; i <= num; i++) {
             row += num + ' ';
         }
 
-        for (let j = 1; j <= num; j++) {
-            print(row);
-        }
+        return row;
     }
 
     function print(message) {
