9ba34c8775de7282f53f12247978aa393c5959ca
Nicole <nicole.georgievva@gmail.com>
2022-07-12 19:55:19 +0300

    Add `perfectNumber` task solution

---
+++ functions/homework/perfectNumber.js
@@ -0,0 +1,36 @@
+function perfectNumber(number) {
+    let num = Math.floor(number);
+    let sum = 0;
+
+    if (num < 0) {
+        print("It's not so perfect.");
+    }
+
+    sumOfPositiveDivisors(num);
+
+
+
+    function sumOfPositiveDivisors(num) {
+
+        for (let i = 1; i < num; i++) {
+            let currentDivisor = i;
+            let division = num / currentDivisor;
+
+            if (division % 1 === 0) {
+                sum += currentDivisor;
+            }
+        }
+
+        if (sum === num) {
+            print("We have a perfect number!");
+        } else {
+            print("It's not so perfect.");
+        }
+    }
+
+    function print(message) {
+        console.log(message);
+    }
+}
+
+perfectNumber(6);
