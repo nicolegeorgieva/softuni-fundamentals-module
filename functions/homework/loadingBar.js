2456850d8bf4021d7a83bab0694564f6893ca16e
Nicole <nicole.georgievva@gmail.com>
2022-07-13 17:47:44 +0300

    Add `loadingBar` task solution

---
+++ functions/homework/loadingBar.js
@@ -0,0 +1,36 @@
+function loadingBar(number) {
+
+    if (number !== 100) {
+        let arr = [];
+        let final = [];
+
+        for (let i = 1; i <= number; i += 10) {
+
+            arr += '%';
+        }
+
+        if (arr !== '%%%%%%%%%%') {
+            for (let j = number; j < 100; j += 10) {
+                final += '.';
+            }
+        }
+
+        print(`${number}% [${arr}${final}]`);
+        print('Still loading...');
+
+    } else {
+        print('100% Complete!');
+    }
+
+
+    function print(message) {
+        console.log(message);
+    }
+}
+
+loadingBar(30); //(0, 10, 20, 30...)
+
+//30% [%%%.......]
+//Still loading...
+
+//100% Complete!
