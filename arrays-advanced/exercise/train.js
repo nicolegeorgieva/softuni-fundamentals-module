35c91c8ab602320d1e578c1307b9d9c45ae45968
Nicole <nicole.georgievva@gmail.com>
2022-07-14 22:23:12 +0300

    Add `train` task

---
+++ arrays-advanced/exercise/train.js
@@ -0,0 +1,8 @@
+function train(input) {
+    let wagons = input[0];
+    let maxCapacityOfAWagon = Number(input[1]);
+
+    //T0-DO
+}
+
+train(['32 54 21 12 4 0 23', '75', 'Add 10', 'Add 0', '30', '10', '75']);
